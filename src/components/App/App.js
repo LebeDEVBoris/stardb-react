c5a80e03aa712562ff90467579e0a7f0288642b1
LebeDEV Boris <global-mangal@yandex.ru>
2020-03-25 23:31:51 +0500

    Deleting main files, will made project later

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-import './../../reset.css';
-import './../../common.sass';
+// Common styles
+import './../../common.sass'
 
-import  Header from './../Header/Header';
-import RandomPlanet from './../RandomPlanet/RandomPlanet';
-import PeopleList from './../PeopleList/PeopleList';
-import PersonDetails from './../PeopleDetails/PeopleDetails';
+const App = () => {
 
-export default class App extends Component {
-
-    render() {
-
-        // https://swapi.co/api/people/7/
-        
-        return(
-            <div className="app">
-                <Header />
-                <RandomPlanet />
-                <div className="peopleInfo">
-                    <PeopleList />
-                    <PersonDetails id={2}/>
-                </div>
-            </div>
-        );
-    }
+    return(
+        <h1>App</h1>
+    );
 }
+
+export default App;
